9d15987494f14f2a46752559815f0129e8dcb70d
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-05-07 14:52:14 -0300

    Alteracoes feitas de observacao

--- Codigos-AbertosJAVA/src/lambdas/CalculoTeste2.java
+++ Codigos-AbertosJAVA/src/lambdas/CalculoTeste2.java
@@ -12,9 +12,9 @@ public class CalculoTeste2 {
 
 		/*
 		 * Outra forma de fazer lambda é SEM A INCLUSÃO DA PALAVRA 'RETURN', basta não
-		 * colocar as chaves '{}', pois ele automaticamente irá retornar o valor. SÓ É
-		 * VÁLIDO EM APENAS UMA ÚNICA SENTENÇA DE CÓDIGO, POIS SE FOR MAIS DE UMA DÁ
-		 * ERRO E É NECESSÁRIO COLOCAR AS CHAVES.
+		 * colocar as chaves '{}', pois ele automaticamente irá retornar o valor. ISSO
+		 * SÓ É VÁLIDO EM APENAS UMA ÚNICA SENTENÇA DE CÓDIGO, POIS SE FOR MAIS DE UMA
+		 * DÁ ERRO E É NECESSÁRIO COLOCAR AS CHAVES.
 		 */
 		calc = (x, y) -> x * y;
 		System.out.println(calc.executar(2, 3));
