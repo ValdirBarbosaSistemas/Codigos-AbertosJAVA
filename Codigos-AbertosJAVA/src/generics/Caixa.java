2bd10cf7ed6bd24955cda33e5a8f8cf30f58905c
Valdir Barbosa da Silva Junior <valdir.barbosasistemas@gmail.com>
2020-06-18 19:54:44 -0300

    Alteração en Generics

--- Codigos-AbertosJAVA/src/generics/Caixa.java
+++ Codigos-AbertosJAVA/src/generics/Caixa.java
@@ -5,7 +5,8 @@ public class Caixa<TIPO> {
 	 * Agora com Generics, vamos colocar um tipo genérico mas que não vai ser
 	 * Inicializado/Definido agora dentro da classe, será inicializado depois...
 	 * Para isso, basta colocarmos o sinal de '<>' ao lado da classe e também um
-	 * NOME QUALQUER DE SUA PREFERÊNCIA.
+	 * NOME QUALQUER DE SUA PREFERÊNCIA, ou seja, primeiro é criado o generics em
+	 * uma classe para DEPOIS ser chamado.
 	 */
 	private TIPO coisa;
 
