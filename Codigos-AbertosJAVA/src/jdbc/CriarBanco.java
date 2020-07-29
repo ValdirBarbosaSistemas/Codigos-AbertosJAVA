5e8bb42d5be0dbc968bbbd62fccfa5aa107f951d
Valdir Barbosa da Silva Junior <valdir.barbosasistemas@gmail.com>
2020-07-29 20:41:32 -0300

    Alteração e criação da classe
    Alteração na classe CriarBanco e criação da FábricaConexao

--- Codigos-AbertosJAVA/src/jdbc/CriarBanco.java
+++ Codigos-AbertosJAVA/src/jdbc/CriarBanco.java
@@ -7,8 +7,11 @@ import java.sql.Statement;
 
 public class CriarBanco {
 	public static void main(String[] args) throws SQLException {
+		// Variável para o endereço do banco
 		final String url = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
+		// Variável para o usuário de acesso
 		final String usuario = "root";
+		// Variável para a senha de acesso
 		final String senha = "V@ldir";
 		Connection conexao = DriverManager.getConnection(url, usuario, senha);
 
