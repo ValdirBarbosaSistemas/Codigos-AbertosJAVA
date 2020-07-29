ebc259b4f0290767628b398bff6f23a8400dc93c
Valdir Barbosa da Silva Junior <valdir.barbosasistemas@gmail.com>
2020-07-29 12:53:23 -0300

    Alteração de arquivo e criação de outro
    Mudanças na classe TesteConexao e criaçao do banco de dados

--- Codigos-AbertosJAVA/src/jdbc/TesteConexao.java
+++ Codigos-AbertosJAVA/src/jdbc/TesteConexao.java
@@ -9,8 +9,9 @@ public class TesteConexao {
 		// Criando uma conexão
 		final String url = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
 		/*
-		 * jdbc = em que local vou estar acessando o banco de dados? mysql = Nome do
-		 * drive que vai acessar localhost = nome do servidor
+		 * jdbc = em que local vou estar acessando o banco de dados 
+		 * mysql = Nome do drive que vai acessar 
+		 * localhost = nome do servidor
 		 */
 		final String usuario = "root";
 		final String senha = "V@ldir";
@@ -20,7 +21,7 @@ public class TesteConexao {
 		/*
 		 * Neste caso aprendemos que isso é um método que vai lançar uma exeção checada,
 		 * ou seja, teremos que tratar, que no caso teria que ser com um try/catch ou
-		 * poderíamos lançar a mesma para 'outra' pessoal poder tratar
+		 * poderíamos lançar a mesma para 'outra' pessoa poder tratar
 		 */
 
 		System.out.println("Conexão realizada com sucesso!");
