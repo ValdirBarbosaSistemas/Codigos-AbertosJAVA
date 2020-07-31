600770a8fff978ab4d0def0caf42418f0c2082fa
Valdir Barbosa da Silva Junior <valdir.barbosasistemas@gmail.com>
2020-07-31 12:50:43 -0300

    Alterações em classe FabricaConexao e Criação da tabela
    Criação da tabela pessoa e alteração na classe FabricaConexao

--- Codigos-AbertosJAVA/src/jdbc/FabricaConexao.java
+++ Codigos-AbertosJAVA/src/jdbc/FabricaConexao.java
@@ -5,9 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class FabricaConexao {
+	/*
+	 * Criação de um método estático na classe 'FabricaConexao' para que quando
+	 * sempre criar uma conexão ou algo do tipo, chamar o método em vez de ficar
+	 * sempre criando em uma classe
+	 */
 	public static Connection getConexao() {
 		try {
-			final String url = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
+			final String url = "jdbc:mysql://localhost/curso_java?verifyServerCertificate=false&useSSL=true";
 			final String usuario = "root";
 			final String senha = "V@ldir";
 			return DriverManager.getConnection(url, usuario, senha);
